a0b1639b77a058959da45ed60d3bf59f01de95e2
Daniel Shields <daniel@karuna.co.za>
2023-09-28 16:29:40 +0200

    Add transformers code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, session } = require('electron');
 const path = require('path');
+const { run } = require('./model.js');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -9,8 +10,8 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1600,
+    height: 768,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
@@ -26,7 +27,25 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+
+  // Add a handler for the `transformers:run` event. This enables 2-way communication
+  // between the renderer process (UI) and the main process (processing).
+  // https://www.electronjs.org/docs/latest/tutorial/ipc#pattern-2-renderer-to-main-two-way
+  ipcMain.handle('transformers:run', run)
+
+  createWindow();
+
+  // Define a custom Content Security Policy to only allow loading resources from the app's origin.
+  // session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+  //   callback({
+  //     responseHeaders: {
+  //       ...details.responseHeaders,
+  //       'Content-Security-Policy': ["default-src 'self'"]
+  //     }
+  //   })
+  // })
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
